af82ce2c3e88260317b688bce025fc5212eec692
martpec <95352759+martpec@users.noreply.github.com>
2024-02-19 15:35:41 +0100

    Add(), Delte()

--- ContactBook.cs
+++ ContactBook.cs
@@ -4,7 +4,31 @@ namespace ContactManagementSystem
     {
         private readonly List<Contact> contacts = new();
 
+        private int id = 0;
         // Implement methods for managing contacts (add, view, update, delete)
+        public void Add()
+        {            
+            Console.Write("Name: ")
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Email: ")
+            string email = Console.ReadLine();   
+            Console.WriteLine();
+            Console.Write("Phone number: ")
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine();
+
+            Contact newContact = new Contact(id, name, mail, phoneNumber);
+            contacts.Add(newContact);
+            id++;   
+        }
+
+
+
+        public void Delete()
+        {
+            
+        }
     }
 
 }
