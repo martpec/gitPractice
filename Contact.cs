83adffebbb2dd220d9de79ebe562535b450e7a42
martpec <95352759+martpec@users.noreply.github.com>
2024-02-19 14:03:01 +0100

    Contact class

--- Contact.cs
+++ Contact.cs
@@ -2,6 +2,17 @@ namespace ContactManagementSystem
 {
     public class Contact
     {
+        private int ID;
+        private string name;
+        private string mail;
+        private string phoneNumber;
 
+        public Contact(int ID, string name, string mail, string phoneNumber)
+        {
+            this.ID = ID;
+            this.name = name;
+            this.mail = mail;
+            this.phoneNumber = phoneNumber;
+        }
     }
 }
